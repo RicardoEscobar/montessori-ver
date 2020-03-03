d48f74e316f34fc356fe15ece6ee69a740deb24a
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2020-03-03 09:09:47 -0600

    Typo constantes del mismo nombre js/email.js

--- js/email.js
+++ js/email.js
@@ -4,10 +4,14 @@ const validationNombre = document.getElementById('validationNombre');
 const validationEmail = document.getElementById('validationEmail');
 const validationTelefono = document.getElementById('validationTelefono');
 const gridRadios1 = document.getElementById('gridRadios1');
-const gridRadios1 = document.getElementById('gridRadios2');
-const gridRadios1 = document.getElementById('gridRadios3');
+const gridRadios2 = document.getElementById('gridRadios2');
+const gridRadios3 = document.getElementById('gridRadios3');
 
 
-enviar.onclick = function(){
-    console.log(validationNombre);
+function creaLigaEmail(sendToEmail) {
+    let ligaEmail = "mailto:" + sendToEmail;
+    ligaEmail + validationEmail.value ? validationEmail.value : "";
+    ligaEmail + validationEmail.value ? validationEmail.value : "";
+
+    console.log(ligaEmail);
 }
