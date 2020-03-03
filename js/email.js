406958612aa8b83e2a63b1ae378b4f2535124c15
Ricardo Escobar <jorge.ricardo.escobar@gmail.com>
2020-03-03 08:53:33 -0600

    Agrega js/email.js

---
+++ js/email.js
@@ -0,0 +1,13 @@
+// selecciona los elementos necesarios para el correo
+const enviar = document.getElementById('enviar');
+const validationNombre = document.getElementById('validationNombre');
+const validationEmail = document.getElementById('validationEmail');
+const validationTelefono = document.getElementById('validationTelefono');
+const gridRadios1 = document.getElementById('gridRadios1');
+const gridRadios1 = document.getElementById('gridRadios2');
+const gridRadios1 = document.getElementById('gridRadios3');
+
+
+enviar.onclick = function(){
+    console.log(validationNombre);
+}
